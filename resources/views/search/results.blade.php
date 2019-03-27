b15c321992478e62e8fee2bfeb13cda874a836f3
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-03-27 12:56:34 +0100

    Update results.blade.php

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -1,10 +1,29 @@
-
 <html>
     <body>   
         <?php
-             $aboutcontent = file_get_contents('C:\xampp\htdocs\mylara\resources\views\pages\about.blade.php', true);//returns page content as string
-             $found = strpos($aboutcontent, $_GET["name"]);
-            echo $found;
+
+                    //turns page content into string
+                $aboutcontent = file_get_contents('C:\xampp\htdocs\mylara\resources\views\pages\about.blade.php', true);
+                    //input field   
+                $input = $_GET["name"];
+
+            if (empty($_GET["name"])) {
+                echo "Please search for something!";
+                }
+
+            else {
+                    //find the number of into input-field inserted string
+                $found = substr_count($aboutcontent, $input);
+
+                    //find (canse insensitive)string in a string
+                $matches = stristr($aboutcontent, $_GET["name"]);
+                //$matches = strpbrk($aboutcontent, $_GET["name"]);
+
+                echo "Found: " . $found . "<br>";
+            
+                    //display the results in string from the 1st till 100 catarcter
+                echo substr($matches, 0, 100)  . "....";
+            }
         ?>              
     </body>
 </html>
